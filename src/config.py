02eecdc82b0b8a1cc7f4c7e246b46479fe79acb9
Horrorzero <vladpushkar82@gmail.com>
2026-08-09 18:01:26 +0200

    fix10

--- src/config.py
+++ src/config.py
@@ -1,6 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+
+    CLIENT_ID: int = 1  # Unique identifier for the client instance
     # API Client / Backend connection
     core_api_url: str = "http://admin_api:8000"
 
